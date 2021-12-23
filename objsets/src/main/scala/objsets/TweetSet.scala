e670c5ba1ca923a087e080599700cb94efd5f9a7
Ryuta Oshikiri <ryushikiri@gmail.com>
2021-12-22 19:15:26 -0800

    Add tweet filter and union

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -212,14 +212,18 @@ object GoogleVsApple:
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filter(
+    tweet => google.exists(
+      key => tweet.text.contains(key)
+    )
+  )
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filter(tweet => apple.exists(key => tweet.text.contains(key)))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = (googleTweets union appleTweets).descendingByRetweet
 
 object Main extends App:
   // Print the trending tweets
